afecae37a03786008881d8b22ec85e6027e1cc20
GET Academy <terje@kolderup.net>
2019-06-13 14:56:39 +0200

    Update Box.cs

--- TilfeldigeFirkanter/Box.cs
+++ TilfeldigeFirkanter/Box.cs
@@ -4,12 +4,12 @@ namespace TilfeldigeFirkanter
 {
     public class Box
     {
-        public int X { get; private set; }
-        public int Y { get; private set; }
+        public int X { get; }
+        public int Y { get; }
         public int StartY => Y;
         public int EndY => Y + Height;
-        public int Width { get; private set; }
-        public int Height { get; private set; }
+        public int Width { get; }
+        public int Height { get; }
         private int _minimumSize = 3;
 
         public Box(Random random, int maxX, int maxY)
@@ -18,14 +18,6 @@ namespace TilfeldigeFirkanter
             Height = random.Next(_minimumSize, maxY);
             X = random.Next(1, maxX - Width);
             Y = random.Next(1, maxY - Height);
-        }
-
-        public Box(int x, int y, int width, int height)
-        {
-            X = x;
-            Y = y;
-            Width = width;
-            Height = height;
         }
     }
 }
